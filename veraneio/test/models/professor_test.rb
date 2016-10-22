5ba13f7359881fcf4acc6d761b594048b9a9d417
rickftime <rickftime@gmail.com>
2016-10-22 18:03:04 -0200

    Fix dos casos de teste, build agora está passando
    	- Para testes com herança, a definição da instância deve ser
    	  sempre na class pai.
    	- Removido arquivos desnecessários
    	- Refatorização dos casos de testes anteriores

--- veraneio/test/models/professor_test.rb
+++ veraneio/test/models/professor_test.rb
@@ -4,10 +4,5 @@ class ProfessorTest < ActiveSupport::TestCase
 	def setup
 		@professor = Professor.new nome: "Teste", email:"teste@email.com"
 	end
-
-   test "professor não pode ser criado sem nome" do
-	   assert @professor.valid?
-	   @professor.nome = nil
-	   assert_not @professor.valid?
-   end
 end
+
--- veraneio/test/models/trabalho_test.rb
+++ veraneio/test/models/trabalho_test.rb
@@ -2,11 +2,16 @@ require 'test_helper'
 
 class TrabalhoTest < ActiveSupport::TestCase
 	def setup
-		@trabalho = trabalhos(:one)
+		@trabalho = trabalhos(:trabalho_1)
 	end
 
 	test 'trabalho nao pode ser criado sem titulo' do
 		@trabalho.titulo = nil
+		assert_not @trabalho.valid?
+	end
+
+	test 'trabalho nao pode ser criado sem orientador' do
+		@trabalho.orientador = nil
 		assert_not @trabalho.valid?
 	end
 end
