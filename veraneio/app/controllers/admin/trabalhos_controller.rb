ae464704a85af8d926cf79f36a5ecb30cd1bdc35
saviocamacam <savio.camacam@hotmail.com>
2016-11-04 05:11:00 -0200

    A informacao de estado agora e informada de acordo o valor no trabalho respeitando as cores devidas. O botao de enviar para avaliacao ja recupera os objetos de professor que precisam receber o e-mail

--- veraneio/app/controllers/admin/trabalhos_controller.rb
+++ veraneio/app/controllers/admin/trabalhos_controller.rb
@@ -12,11 +12,17 @@ class Admin::TrabalhosController < ApplicationController
 
     def update
         @orientador = Professor.where(email: params[:orientador]).first
-		@banca_1 = Professor.where(email: params[:banca_1]).first
-		@banca_2 = Professor.where(email: params[:banca_2]).first
+        @banca_1 = Professor.where(email: params[:banca_1]).first
+        @banca_2 = Professor.where(email: params[:banca_2]).first
+        @trabalho = Trabalho.find params[:id]
+        
+        if @trabalho.estado == "Recebido do Aluno"
+            if @banca_1 != @banca_2 
+                #Dani, divirta-se aqui
 
-		@trabalho = Trabalho.find params[:id]
-		@trabalho.update estado: "Enviado para Avaliação"
+                @trabalho.update estado: "Enviado para Avaliação"
+            end
+        end
 
         redirect_to admin_trabalhos_path
     end
