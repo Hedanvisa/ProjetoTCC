24c3442ccbf51a98755f389d21eb73ddfefe376e
Henrique Pinheiro <rickftime@gmail.com>
2016-11-04 01:17:37 -0200

    Implementada a troca de arquivo de trabalho para Estudante
    	- Validacao de banca_1 agora aceita email nulo
    	- Form de trabalho agora exibe o link e nome do arquivo no
    	banco de dados se existir
    	- Removido codigo duplicado no controller Trabalhos
    	- Criado a view para a action Edit em Trabalho

--- veraneio/app/models/usuario.rb
+++ veraneio/app/models/usuario.rb
@@ -1,3 +1,4 @@
 class Usuario < ActiveRecord::Base
-	validates :nome, presence: true, uniqueness: true
+	validates_uniqueness_of :nome, allow_blank: true, allow_nil: true
+	validates_uniqueness_of :email, allow_blank: true, allow_nil: true
 end
