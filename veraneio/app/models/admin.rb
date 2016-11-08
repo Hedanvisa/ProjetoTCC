557741d43ba698b03abff862bb161f406f13d330
wagjub <danielcostavalerio@hotmail.com>
2016-11-08 15:03:28 -0200

    Model Periodo closes #19

--- veraneio/app/models/admin.rb
+++ veraneio/app/models/admin.rb
@@ -2,4 +2,12 @@ class Admin < Usuario
 	validates :nome, presence: true
 	validates :email, presence: true
 	has_secure_password
+
+def self.table_name
+	'admin'
 end
+
+end
+			
+
+
--- veraneio/db/schema.rb
+++ veraneio/db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161107055309) do
+ActiveRecord::Schema.define(version: 20161108164332) do
+
+  create_table "admin_periodos", force: :cascade do |t|
+    t.datetime "inicio"
+    t.datetime "termino"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "trabalhos", force: :cascade do |t|
     t.string   "titulo"
@@ -44,7 +51,6 @@ ActiveRecord::Schema.define(version: 20161107055309) do
     t.string   "type"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "estado"
     t.string   "estado_acesso"
   end
 
