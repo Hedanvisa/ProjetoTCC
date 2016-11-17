a7cd7d864740f53d1c94a94b183ba916c27c5abf
Henrique Pinheiro <rickftime@gmail.com>
2016-11-17 21:49:16 -0200

    Fix de teste do Periodo
    	- O teste estava tentando inserir um periodo com tempo anterior
    	aos periodos ja cadastrados em periodos.yml

--- veraneio/test/controllers/admin/periodos_controller_test.rb
+++ veraneio/test/controllers/admin/periodos_controller_test.rb
@@ -1,44 +1,46 @@
 require 'test_helper'
 
 class Admin::PeriodosControllerTest < ActionController::TestCase
-  def setup
-   @admin_periodo = periodos(:one)
-  end
+	fixtures :periodos
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:admin_periodos)
-  end
+	def setup
+		@admin_periodo = periodos(:one)
+	end
 
-  test "should create admin_periodo" do
-    assert_difference('Periodo.count') do
-      post :create, periodo: { inicio: @admin_periodo.inicio, termino: @admin_periodo.termino }
-    end
+	test "should get index" do
+		get :index
+		assert_response :success
+		assert_not_nil assigns(:admin_periodos)
+	end
 
-    assert_redirected_to admin_periodos_path
-  end
+	test "should create admin_periodo" do
+		assert_difference('Periodo.count') do
+			post :create, periodo: { inicio: "2020-1-1 20:30:00", termino: "2021-1-1 20:30:00" }
+		end
 
-  test "should show admin_periodo" do
-    get :show, id: @admin_periodo
-    assert_response :success
-  end
+		assert_redirected_to admin_periodos_path
+	end
 
-  test "should get edit" do
-    get :edit, id: @admin_periodo
-    assert_response :success
-  end
+	test "should show admin_periodo" do
+		get :show, id: @admin_periodo
+		assert_response :success
+	end
 
-  test "should update admin_periodo" do
-    patch :update, id: @admin_periodo, periodo: { inicio: @admin_periodo.inicio, termino: @admin_periodo.termino }
-    assert_redirected_to admin_periodo_path(assigns(:admin_periodo))
-  end
+	test "should get edit" do
+		get :edit, id: @admin_periodo
+		assert_response :success
+	end
 
-  test "should destroy admin_periodo" do
-    assert_difference('Periodo.count', -1) do
-      delete :destroy, id: @admin_periodo
-    end
+	test "should update admin_periodo" do
+		patch :update, id: @admin_periodo, periodo: { inicio: @admin_periodo.inicio, termino: @admin_periodo.termino }
+		assert_redirected_to admin_periodo_path(assigns(:admin_periodo))
+	end
 
-    assert_redirected_to admin_periodos_path
-  end
+	test "should destroy admin_periodo" do
+		assert_difference('Periodo.count', -1) do
+			delete :destroy, id: @admin_periodo
+		end
+
+		assert_redirected_to admin_periodos_path
+	end
 end
