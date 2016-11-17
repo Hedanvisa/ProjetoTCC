20efee5a9cb2bbafb38f81754a5e59f4296e183d
wagjub <danielcostavalerio@hotmail.com>
2016-11-17 20:24:24 -0200

    arrumando o helper de periodo

--- veraneio/app/helpers/admin/periodos_helper.rb
+++ veraneio/app/helpers/admin/periodos_helper.rb
@@ -1,6 +1,6 @@
 module Admin::PeriodosHelper
 	def esta_periodo_aberto?
-		if Time.now < Periodo.last.termino
+		if Time.now < Periodo.order(:termino).last.termino
 			return true
 		else
 			return false
